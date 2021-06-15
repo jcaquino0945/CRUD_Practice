b5d0802197087c41bccddda0b965a2e41e17f7f6
Miguel Aquino <jcaquino0945@gmail.com>
2021-06-15 23:10:34 +0800

    home setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { DiaryComponent } from './diary/diary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PagenotfoundComponent,
+    DiaryComponent
   ],
   imports: [
     BrowserModule,
